e4b698fdc329bd6c858a70d54ec334a4334a517b
Simon Hrabec <simon.hrabec@gmail.com>
2023-04-04 00:41:29 +0900

    remove unecessary case in the error manager

--- prusti-viper/src/encoder/errors/error_manager.rs
+++ prusti-viper/src/encoder/errors/error_manager.rs
@@ -699,6 +699,17 @@ impl<'tcx> ErrorManager<'tcx> {
                 )
             }
 
+            ("refute.failed:refutation.true", ErrorCtxt::Panic(PanicCause::Refute)) => {
+                PrustiError::verification(
+                    "the refuted expression holds in all cases or could not be reached",
+                    error_span,
+                )
+            }
+
+            ("refute.failed:refutation.true", ErrorCtxt::UnreachableCode) => {
+                PrustiError::warning("Detected unreachable code", error_span)
+            }
+
             (full_err_id, ErrorCtxt::Unexpected) => {
                 PrustiError::internal(
                     format!(
